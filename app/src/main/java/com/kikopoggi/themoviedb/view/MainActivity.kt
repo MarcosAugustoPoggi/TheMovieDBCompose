f26a31cfce79565f181f2eab6af7800ff7c61f00
Marcos A Poggi <kikopoggi@gmail.com>
2021-07-04 12:25:37 -0300

    Add transition Animation

--- app/src/main/java/com/kikopoggi/themoviedb/view/MainActivity.kt
+++ app/src/main/java/com/kikopoggi/themoviedb/view/MainActivity.kt
@@ -2,6 +2,9 @@ package com.kikopoggi.themoviedb.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.kikopoggi.themoviedb.R
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -17,7 +20,29 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.fragmentFactory = fragmentFactory
         setContentView(R.layout.activity_main)
 
-        supportActionBar?.hide()
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.movieListFragment) {
+                supportActionBar?.setDisplayShowTitleEnabled(true)
+                supportActionBar?.setTitle("TheMovieDB")
+            } else  {
+                supportActionBar?.setDisplayShowTitleEnabled(false)
+                supportActionBar?.show()
+            }
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
